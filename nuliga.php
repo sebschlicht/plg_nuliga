13370f5498265992bf62f59df63079d352ba17eb
Sebastian Schlicht <sebastian@jablab.de>
2017-11-06 16:37:51 +0100

    connection to com_nuliga model; error messages; CSS dummy added

--- nuliga.php
+++ nuliga.php
@@ -9,8 +9,24 @@
 // no direct access
 defined( '_JEXEC' ) or die;
 
+// check if com_nuliga is installed and enabled
+if (!JComponentHelper::isEnabled('com_nuliga', true))
+{
+    throw new Exception(JText::_('PLG_CONTENT_NULIGA_ERROR') . ' ' . JText::_('PLG_CONTENT_NULIGA_COMPONENT_NOT_INSTALLED'), 500);
+    return false;
+}
+
+// import required component classes
+JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_nuliga/tables');
+JModelLegacy::addIncludePath(JPATH_SITE.'/components/com_nuliga/models', 'NuLigaModel');
+
 class PlgContentNuLiga extends JPlugin
 {
+    /**
+     * parameter: id
+     */
+    const PARAM_ID = 'id';
+
     /**
      * Load the language file on instantiation. Note this is only available in Joomla 3.1 and higher.
      * If you want to support 3.0 series you must override the constructor
@@ -45,6 +61,8 @@ class PlgContentNuLiga extends JPlugin
             return true;
         }
 
+        $app = JFactory::getApplication();
+
         // scan for plugin usages
         $regex_one		= '/({nuliga\s*)(.*?)(})/si';
         $regex_all		= '/{nuliga\s*.*?}/si';
@@ -52,9 +70,11 @@ class PlgContentNuLiga extends JPlugin
         $numMatches	= preg_match_all($regex_all, $row->text, $matches,PREG_OFFSET_CAPTURE | PREG_PATTERN_ORDER);
 
         // initialize component helpers if necessary
-        // TODO remove what can be done inside there
-        $document = JFactory::getDocument();
-        $document->addStyleSheet(JUri::root() . 'media/com_nuliga/css/nuliga.css');
+        if ($numMatches)
+        {
+            $document = JFactory::getDocument();
+            $document->addStyleSheet(JUri::root() . 'media/plg_content_nuliga/css/nuliga.css');
+        }
 
         // process plugin calls
         for ($i = 0; $i < $numMatches; $i++)
@@ -71,22 +91,49 @@ class PlgContentNuLiga extends JPlugin
             }
 
             // render table
-            $output = '';
-            if (array_key_exists('id', $nuliga_params))
+            if (array_key_exists($this::PARAM_ID, $nuliga_params))
             {
-                // TODO render table via frontend model
-                $output = '<p class="nuliga">I will render! But not today...</p>';
+                $id = $nuliga_params[$this::PARAM_ID];
+                $table = $this->loadNuLigaTable($id);
+                if ($table)
+                {
+                    $output = $table->title;
 
-                // TODO add error if table id unknown / rendering failed
+                    // TODO render table via frontend model
+
+                    $row->text = preg_replace($regex_all, $output, $row->text, 1);
+                }
+                else
+                {
+                    // add error if table unknown
+                    $app->enqueueMessage(JText::sprintf('PLG_CONTENT_NULIGA_ERROR_PARAM', $nuliga) . ' '
+                        . JText::sprintf('PLG_CONTENT_NULIGA_TABLE_UNKNOWN', $nuliga_params[$this::PARAM_ID]), 'warning');
+                }
             }
             else
             {
-                // TODO add error if table id missing
-                $output = 'Error: NuLiga table id missing!';
+                // add error if table id missing
+                $app->enqueueMessage(JText::sprintf('PLG_CONTENT_NULIGA_ERROR_PARAM', $nuliga) . ' '
+                    . JText::sprintf('PLG_CONTENT_NULIGA_PARAM_MISSING', 'id'), 'error');
             }
-            $row->text = preg_replace($regex_all, $output, $row->text, 1);
         }
 
         return true;
 	}
+
+	protected function loadNuLigaTable($id)
+    {
+        // load table via component model
+        $model = JModelLegacy::getInstance('NuLiga', 'NuLigaModel');
+        return $model->loadNuLigaTable($id);
+
+        // build table query
+        //$jtable = JTable::getInstance('NuLiga', 'NuLigaTable', array());
+        //return $jtable->load($id) ? $jtable : false;
+    }
+
+    protected function renderNuLigaTable($table)
+    {
+        // TODO
+    }
 }
